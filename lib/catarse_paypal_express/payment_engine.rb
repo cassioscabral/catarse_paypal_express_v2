bc8633a987c262fde9eabc0e4b4cb47eb63969d3
Antônio Roberto Silva <forevertonny@gmail.com>
2014-03-18 14:04:19 -0300

    we dont need to use interface anymore

--- lib/catarse_paypal_express/payment_engine.rb
+++ lib/catarse_paypal_express/payment_engine.rb
@@ -1,29 +1,25 @@
-begin
-  module CatarsePaypalExpress
-    class PaymentEngine < PaymentEngines::Interface
+module CatarsePaypalExpress
+  class PaymentEngine
 
-      def name
-        'PayPal'
-      end
-
-      def review_path contribution
-        CatarsePaypalExpress::Engine.routes.url_helpers.review_paypal_express_path(contribution)
-      end
-
-      def can_do_refund?
-        true
-      end
-
-      def direct_refund contribution
-        CatarsePaypalExpress::ContributionActions.new(contribution).refund
-      end
-
-      def locale
-        'en'
-      end
-
+    def name
+      'PayPal'
     end
+
+    def review_path contribution
+      CatarsePaypalExpress::Engine.routes.url_helpers.review_paypal_express_path(contribution)
+    end
+
+    def can_do_refund?
+      true
+    end
+
+    def direct_refund contribution
+      CatarsePaypalExpress::ContributionActions.new(contribution).refund
+    end
+
+    def locale
+      'en'
+    end
+
   end
-rescue Exception => e
-  puts "Error while use payment engine interface: #{e}"
 end
